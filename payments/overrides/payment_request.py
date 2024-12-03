f0f9d29bbf523e15d090e1dde1da38d87b445f95
Hendar <hendarsundana@gmail.com>
2024-12-04 04:45:33 +0700

    feat: override function original frappe for payment_reqeuset.resend_email to payments.overrides.payment_request.resend_payment_email

--- payments/overrides/payment_request.py
+++ payments/overrides/payment_request.py
@@ -2,7 +2,7 @@ import frappe
 
 @frappe.whitelist(allow_guest=True)
 def resend_payment_email(docname):
-        # Mengambil dokumen "Order Payment Request" berdasarkan docname
+    # Mengambil dokumen "Order Payment Request" berdasarkan docname
     order_payment_request = frappe.get_doc("Order Payment Request", docname)
     # Memanggil metode send_email pada dokumen yang diambil
     return order_payment_request.send_email()
